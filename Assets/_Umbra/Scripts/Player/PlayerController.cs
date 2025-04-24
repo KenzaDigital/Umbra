e641055dbb6bf02e503392c5e286fe6846128575
KenzaDigital <kenza.chabane4@gmail.com>
2025-04-24 09:56:59 +0200

    RunCoroutine&CoolDownCoroutine
    The player has a limited running time of around 3 seconds, then when the coroutine stops, they have a 5-second cooldown before being able to run again

--- Assets/_Umbra/Scripts/Player/PlayerController.cs
+++ Assets/_Umbra/Scripts/Player/PlayerController.cs
@@ -1,19 +1,25 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
+using System.Collections;
 
 public class PlayerController : MonoBehaviour
 {
     [SerializeField] private float moveSpeed = 5f;
+    [SerializeField] private float originalSpeed; // Vitesse normale
+    [SerializeField] private float runSpeed = 10f;
+    [SerializeField] private float runDuration = 3f;
+    [SerializeField] private float runCooldown = 10f;
 
     private Vector2 move;
     private Rigidbody2D rb;
+    private bool canRun = true;
+    private bool isRunning = false;
 
     private void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
     }
 
-    // Méthode appelée par le système d'Input
     public void Move(InputAction.CallbackContext context)
     {
         move = context.ReadValue<Vector2>();
@@ -21,32 +27,51 @@ public class PlayerController : MonoBehaviour
 
     public void Run(InputAction.CallbackContext context)
     {
-        if (context.performed)
+        // Vérifie si l'entrée est "performed" et si le joueur peut courir
+        if (context.performed && canRun)
         {
-            moveSpeed *= 2; // Double la vitesse de déplacement
+            StartCoroutine(RunCoroutine());
         }
-        else if (context.canceled)
-        {
-            moveSpeed /= 2; // Rétablit la vitesse de déplacement
-        }
+    }
+
+    private IEnumerator RunCoroutine()
+    {
+        Debug.Log("RunCoroutine started");
+        canRun = false; // Désactiver la possibilité de courir
+        isRunning = true; // Indiquer que le joueur est en train de courir
+
+        originalSpeed = moveSpeed; // Sauvegarder la vitesse normale
+        moveSpeed = runSpeed; // Passer à la vitesse de course
+
+        yield return new WaitForSeconds(runDuration); // Attendre la durée de la course
+
+        isRunning = false; // Indiquer que le joueur a terminé de courir
+        moveSpeed = originalSpeed; // Rétablir la vitesse normale
+        Debug.Log("RunCoroutine: Speed reset to original speed");
+
+        // Lancer le cooldown après la course
+        StartCoroutine(CooldownCoroutine());
+    }
+
+    private IEnumerator CooldownCoroutine()
+    {
+        Debug.Log("Cooldown started");
+        yield return new WaitForSeconds(runCooldown); // Attendre le délai de récupération
+        canRun = true; // Réactiver la possibilité de courir
+        Debug.Log("Cooldown ended");
     }
 
     private void Update()
     {
-        // Vérifier si le joueur est en mouvement
         if (move != Vector2.zero)
         {
-            // Calculer l'angle de rotation en fonction de la direction du mouvement
             float angle = Mathf.Atan2(move.y, move.x) * Mathf.Rad2Deg;
-
-            // Appliquer la rotation au Transform du joueur
             transform.rotation = Quaternion.Euler(0, 0, angle);
         }
     }
 
     private void FixedUpdate()
     {
-        // Appliquer le mouvement
         Vector2 movement = move * moveSpeed * Time.fixedDeltaTime;
         rb.MovePosition(rb.position + movement);
     }
